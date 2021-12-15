ee3e43bd208a458e4c3f940641563d6b458c4180
DK <>
2021-12-15 16:51:17 +0500

    Add post method on courses

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const path = require('path')
 const exphbs = require('express-handlebars')
 const { extname } = require('path')
 
+const homeRoutes = require('./routes/home')
+const coursesRoutes = require('./routes/courses')
+const addRoutes = require('./routes/add')
+
 const app = express()
 
 const hbs = exphbs.create({
@@ -14,26 +18,13 @@ app.set('view engine', 'hbs') // use handlebars engine
 app.set('views', 'views') // here we say handlebars where will store view. by default it is views folder
 
 app.use(express.static('public'))
+app.use(express.urlencoded({
+    extended: false
+}))
 
-app.get('/', (req, res) => {
-    res.render('index', {
-        title: 'Главная страница',
-        isHome: true
-
-    })
-})
-app.get('/add', (req, res) => {
-    res.render('add', {
-        title: 'Добавить курс',
-        isAdd: true
-    })
-})
-app.get('/courses', (req, res) => {
-    res.render('courses', {
-        title: 'Курсы',
-        isCourses: true
-    })
-})
+app.use('/', homeRoutes)
+app.use('/courses', coursesRoutes)
+app.use('/add', addRoutes)
 
 const PORT = process.env.PORT || 3000
 app.listen(3000, () => {
---
+++ readme.md
@@ -0,0 +1,3 @@
+### Kill process on port
+PID  -> lsof -t -i:<PORT>
+KILL -> kill -9 <PID>
