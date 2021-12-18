fd1c1187d3a4b246b130253c079b0d68a361ef97
DK <>
2021-12-18 09:51:25 +0500

    Add card view

--- routes/card.js
+++ routes/card.js
@@ -1,22 +1,23 @@
-const {Router} = require('express')
-const Card = require('../models/card')
-const Course = require('../models/course')
-const router = Router()
+const { Router } = require("express");
+const Card = require("../models/card");
+const Course = require("../models/course");
+const router = Router();
 
-router.post('/add', async (req, res) => {
-    const course = await Course.getById(req.body.id)
-    await Card.add(course)
-    res.redirect('/card')
-})
+router.post("/add", async (req, res) => {
+  const course = await Course.getById(req.body.id);
+  await Card.add(course);
+  res.redirect("/card");
+});
 
-router.get('/', async (req, res) => {	
-    const card = await Card.fetch()
-    res.render('card', {
-        title: 'Корзина',
-        isCard: true,
-        courses: card.courses,
-        card
-    })
-})
+router.get("/", async (req, res) => {
+  const card = await Card.fetch();
+  console.log(card);
+  res.render("card", {
+    title: "Корзина",
+    isCard: true,
+    courses: card.courses,
+    card,
+  });
+});
 
-module.exports = router
+module.exports = router;
