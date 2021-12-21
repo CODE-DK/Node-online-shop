2cfbc002b6f42b03e54c336f7c00adc3b884a8f5
DK <>
2021-12-21 18:51:14 +0500

    Add auth middleware

--- routes/cart.js
+++ routes/cart.js
@@ -1,14 +1,15 @@
 const { Router } = require("express");
 const Course = require("../models/course");
 const router = Router();
+const auth = require("../middleware/auth");
 
-router.post("/add", async (req, res) => {
+router.post("/add", auth, async (req, res) => {
   const course = await Course.findById(req.body.id);
   await req.user.addToCart(course);
   res.redirect("/card");
 });
 
-router.delete("/remove/:id", async (req, res) => {
+router.delete("/remove/:id", auth, async (req, res) => {
   await req.user.removeFromCart(req.params.id);
   const user = await req.user.populate("cart.items.courseId");
 
@@ -34,7 +35,7 @@ function computePrice(courses) {
   }, 0);
 }
 
-router.get("/", async (req, res) => {
+router.get("/", auth, async (req, res) => {
   const user = await req.user.populate("cart.items.courseId");
   const courses = mapCartItems(user.cart);
 
